d631188c3196ae192bf694c9636501c2cabc2049
samaunmahmud <samaunmahmud9@gmail.com>
2026-06-20 00:00:46 +0600

    Adding Spring Boot Application entry point

---
+++ src/main/java/com/medclassifier/MedicalClassifierApplication.java
@@ -0,0 +1,12 @@
+package com.medclassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClassifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalClassifierApplication.class, args);
+    }
+}
